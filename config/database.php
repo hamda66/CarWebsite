b352c13502a8d213b1ba9348f3a7d8c6cde87ff9
hamda hassan <hamda6464@gmail.com>
2021-12-28 23:35:43 +0300

    DB fixing config

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysqlite'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
