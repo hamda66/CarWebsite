1919a22507c6f8c3a3234098730ec052aa3a8c72
hamda hassan <hamda6464@gmail.com>
2021-12-29 06:28:36 +0300

    changing db to psql

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-44-193-111-218.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
